02e598d696e4b486148d0193cb919e792fee0e62
happyboy <superhappyboy1995@gmail.com>
2025-10-10 03:56:42 +0800

    update : init

---
+++ src/components/CandleView.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+interface Props {
+}
+
+interface State {
+}
+
+class CandleView extends React.Component<Props, State> {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default CandleView
