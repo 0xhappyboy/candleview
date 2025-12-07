8b269a069c10f8c262e142a21c155cb3a5b4bc82
happyboy <superhappyboy1995@gmail.com>
2025-12-08 00:51:47 +0800

    update : update the en documentation.

--- docs/en/app/_meta.global.tsx
+++ docs/en/app/_meta.global.tsx
@@ -1,30 +1,32 @@
 import { MetaRecord } from "nextra";
 
 const DOCS_ITEMS: MetaRecord = {
-    index: 'Get Started',
-    theme: "Theme",
-    i18n: "I18n",
-    charttype: 'Chart Type',
-    drawing: 'Drawing',
-    graphics: 'Graphics',
-    mainindicators: 'Main Chart Technical Indicators',
-    subindicators: 'Sub Chart Technical Indicators',
-    timeframe: 'Time Frame',
-    timezone: 'Time Zone',
+  index: 'Get Started',
+  theme: "Theme",
+  i18n: "I18n",
+  charttype: 'Chart Type',
+  ai: 'AI',
+  command: 'Command System',
+  drawing: 'Drawing',
+  graphics: 'Graphics',
+  mainindicators: 'Main Chart Technical Indicators',
+  subindicators: 'Sub Chart Technical Indicators',
+  timeframe: 'Time Frame',
+  timezone: 'Time Zone',
 }
 
 export default {
-    index: {
-        type: 'page',
-        theme: {
-            layout: 'full',
-            toc: false,
-            timestamp: false,
-        }
-    },
-    docs: {
-        type: 'page',
-        title: 'Documentation',
-        items: DOCS_ITEMS
-    },
+  index: {
+    type: 'page',
+    theme: {
+      layout: 'full',
+      toc: false,
+      timestamp: false,
+    }
+  },
+  docs: {
+    type: 'page',
+    title: 'Documentation',
+    items: DOCS_ITEMS
+  },
 }
