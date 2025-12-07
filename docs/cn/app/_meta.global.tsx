e31f96c48000426d10069d13fc2fcb854e443751
happyboy <superhappyboy1995@gmail.com>
2025-12-08 01:36:35 +0800

    update : 1.update cn docs.2.delete redundant documents.

--- docs/cn/app/_meta.global.tsx
+++ docs/cn/app/_meta.global.tsx
@@ -1,30 +1,32 @@
 import { MetaRecord } from "nextra";
 
 const DOCS_ITEMS: MetaRecord = {
-    index: 'Get Started',
-    theme: "Theme",
-    i18n: "I18n",
-    charttype: 'Chart Type',
-    drawing: 'Drawing',
-    graphics: 'Graphics',
-    mainindicators: 'Main Chart Technical Indicators',
-    subindicators: 'Sub Chart Technical Indicators',
-    timeframe: 'Time Frame',
-    timezone: 'Time Zone',
+  index: '快速启动',
+  theme: "主题",
+  i18n: "国际化",
+  charttype: '图表类型',
+  ai: 'AI',
+  command: '命令系统',
+  drawing: '绘图',
+  graphics: '图形',
+  mainindicators: '主图技术指标',
+  subindicators: '副图技术指标',
+  timeframe: '时间框架',
+  timezone: '时区',
 }
 
 export default {
-    index: {
-        type: 'page',
-        theme: {
-            layout: 'full',
-            toc: false,
-            timestamp: false,
-        }
-    },
-    docs: {
-        type: 'page',
-        title: 'Documentation',
-        items: DOCS_ITEMS
-    },
+  index: {
+    type: 'page',
+    theme: {
+      layout: 'full',
+      toc: false,
+      timestamp: false,
+    }
+  },
+  docs: {
+    type: 'page',
+    title: '文档',
+    items: DOCS_ITEMS
+  },
 }
